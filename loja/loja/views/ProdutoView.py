8e79a08233f84648868b75fce99d4ac85ce9fa6f
HeitorTB <heitortbr@gmail.com>
2026-05-29 11:41:57 -0300

    Alteração do edit

--- loja/loja/views/ProdutoView.py
+++ loja/loja/views/ProdutoView.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
@@ -10,13 +10,8 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-    #print(produtos)
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
     if produto is not None:
-        produtos = produtos.filter(Produto=produto)
+        produtos = produtos.filter(Produto__contains=produto)
     if promocao is not None:
         produtos = produtos.filter(promocao=promocao)
     if destaque is not None:
@@ -25,8 +20,27 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+            now = timezone.now()
+            now = now - timedelta(days = int(dias))
+            produtos = produtos.filter(criado_em__gte=now)
     if id is not None:
         produtos = produtos.filter(id=id)
     print(produtos)
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+# Adicione para definir o contexto e carregar o template
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='produto/produto.html',
+context=context, status=200)
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context,
+status=200)
 
